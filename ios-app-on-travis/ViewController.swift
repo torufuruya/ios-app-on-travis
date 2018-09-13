966799076e9f5944bf73869b19b18bf6237ae969
Toru <t.furuya825@gmail.com>
2018-09-13 21:49:49 +0900

    Initial commit

---
+++ ios-app-on-travis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-app-on-travis
+//
+//  Created by Toru Furuya on 2018/09/13.
+//  Copyright © 2018 toru.furuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios-app-on-travisTests/ios_app_on_travisTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_app_on_travisTests.swift
+//  ios-app-on-travisTests
+//
+//  Created by Toru Furuya on 2018/09/13.
+//  Copyright © 2018 toru.furuya. All rights reserved.
+//
+
+import XCTest
+@testable import ios_app_on_travis
+
+class ios_app_on_travisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
